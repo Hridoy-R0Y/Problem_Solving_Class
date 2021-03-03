c5507c583e6d96412763925ca9a1f7d99d556750
bu-hridoy <hridoycse14@gmail.com>
2021-03-03 10:38:12 +0600

    pass two parameters to for printing multiplication table 1 to n

--- day1_recursion.cpp
+++ day1_recursion.cpp
@@ -11,7 +11,10 @@ using namespace std;
 void Fun(int i, int n)
 {
     if(i > n) return;
-    cout<<i<<"  I am Hridoy"<<endl;
+    for(int j = 1; j <= 10; j++){
+        cout<<i << " * " << j << " = " << i*j<<endl;
+    }
+    cout<<endl;
     Fun(i + 1 , n);
 }
 
@@ -24,5 +27,13 @@ int main()
     ///    cout<<i<<"  I am Hridoy"<<endl;
     ///}
 
+    /*
+    for(int i = 1; i <= 5; i++){
+        for(int j = 1; j <= 10; j++){
+            cout<<i << " * " << j << " = " << i*j<<endl;
+        }
+        cout<<endl;
+    }
+    */
     Fun(1, n);
 }
