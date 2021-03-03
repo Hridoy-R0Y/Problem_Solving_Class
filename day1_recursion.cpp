b3f0f5494a5feca6336ff5195c2feff635d692e7
bu-hridoy <hridoycse14@gmail.com>
2021-03-03 10:46:18 +0600

    Add a new function that print n to 1 also

--- day1_recursion.cpp
+++ day1_recursion.cpp
@@ -4,36 +4,32 @@
 2. must call recursive function into that function body
 */
 
-
 #include<bits/stdc++.h>
 using namespace std;
 
-void Fun(int i, int n)
+void Fun1(int i, int n)
 {
-    if(i > n) return;
-    for(int j = 1; j <= 10; j++){
-        cout<<i << " * " << j << " = " << i*j<<endl;
+    if(i > n) {
+            cout<<endl;
+            return;
     }
-    cout<<endl;
-    Fun(i + 1 , n);
+    cout<<i<<"+";
+    Fun1(i + 1 , n);
+}
+void Fun2(int i, int n)
+{
+    if(i < n) {
+            cout<<endl;
+            return;
+    }
+    cout<<i<<"+";
+    Fun2(i - 1 , n);
 }
 
 int main()
 {
     int n;
     cin>>n;
-
-    ///for(int i = 1 ; i <= n; i++){
-    ///    cout<<i<<"  I am Hridoy"<<endl;
-    ///}
-
-    /*
-    for(int i = 1; i <= 5; i++){
-        for(int j = 1; j <= 10; j++){
-            cout<<i << " * " << j << " = " << i*j<<endl;
-        }
-        cout<<endl;
-    }
-    */
-    Fun(1, n);
+    Fun1(1, n);
+    Fun2(n,1);
 }
