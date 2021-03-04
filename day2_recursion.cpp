6c1c3731457b0f5420b90f18d0d7590085c985dd
bu-hridoy <hridoycse14@gmail.com>
2021-03-04 10:57:55 +0600

    fibonnacci in day2_recursion.cpp

--- day2_recursion.cpp
+++ day2_recursion.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int func(int x) {
-    if(x <= 0) return 0;
-    return func(x - 1) + x;
+    cout<<x<<endl;
+    if(x == 0) return 0;
+    if(x == 1) return 1;
+    return func(x - 1) + func(x - 2);
 }
 int main() {
     int n;
@@ -12,4 +14,8 @@ int main() {
     cout << c;
     return 0;
 }
+
+/// Tibonacci
+/// 0 1 2 3 6 11 20 37 68 ....... (task1)
+/// 4 -> 12 , 5 -> 23 ......... (task2)
 
